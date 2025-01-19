947bf9086d5833be4e03e52c8681d1a0743ec6d4
Anton <140619997+8nt0n@users.noreply.github.com>
2025-01-19 14:17:29 +0100

    Add files via upload

---
+++ INDEX/AppStoreIndex.js
@@ -0,0 +1,11 @@
+window.addEventListener("DOMContentLoaded", function() {
+
+
+    var AppStoreCool = ["Calender", "Plantera", "Bible", "JS-Clock", "boboclap"]
+
+    var AppStoreGames = []
+
+    var AppStoreTools = []
+
+    var AppStoreMedia = ["streamed"]
+})
